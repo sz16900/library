756f6cca551fad816b11f84a2e076ed3ee771ef1
sz16900 <sz16900@my.bristol.ac.uk>
2020-06-05 12:22:29 -0500

    parsing data from local storage to html index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let myLibrary = [];
+for (let index = 0; index < localStorage.length; index++) {
+  var element = JSON.parse(localStorage.getItem(`book-${index}`));
+  document.write("<p>" + element.title + "</p>");
+}
 
-function Book(title, author, num_pages, read = false) {
+function Book(title, author, num_pages, read = "Unread") {
   this.title = title;
   this.author = author;
   this.num_pages = num_pages;
@@ -11,18 +14,13 @@ Book.prototype.info = function () {
   return `${this.title}, by ${this.author}, ${this.num_pages} pages, read status: ${this.read}`;
 };
 
-function addBookToLibrary(myLibrary, createdBook) {
-  myLibrary.push(createdBook);
+function addBookToLibrary() {
+  let btitle = document.getElementById("btitle").value;
+  let bauthor = document.getElementById("bauthor").value;
+  let bpages = document.getElementById("bpages").value;
+  let bstatus = document.getElementById("bstatus").value;
+
+  newBook = new Book(btitle, bauthor, bpages, bstatus);
+  theKey = localStorage.length;
+  localStorage.setItem(`book-${theKey}`, JSON.stringify(newBook));
 }
-
-theHobbit = new Book("The Hobbit", "J.R.R.Tolkien", 295, true);
-robinHood = new Book(
-  "The Merry Adventures of Robin Hood",
-  "Howard Pyle",
-  295,
-  false
-);
-addBookToLibrary(myLibrary, theHobbit);
-addBookToLibrary(myLibrary, robinHood);
-
-myLibrary.forEach((element) => document.write("<p>" + element.info() + "</p>"));
