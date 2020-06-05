1ecf9eb57b7e297cf9fc687c6848a3dcbf51fae9
sz16900 <sz16900@my.bristol.ac.uk>
2020-06-05 17:02:15 -0500

    toggle read unread

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ function Book(title, author, pages, status = "Unread") {
   this.status = status;
 }
 
+Book.prototype.changeStatus = function () {
+  if (this.status == "Read") {
+    this.status = "Unread";
+  } else {
+    this.status = "Read";
+  }
+};
+
 function addBookToLibrary() {
   let btitle = document.getElementById("btitle").value;
   let bauthor = document.getElementById("bauthor").value;
@@ -21,9 +29,16 @@ function addBookToLibrary() {
 
 function tableCreate() {
   let table = document.getElementById("myTable");
+  let cnt = 1;
   Object.keys(localStorage).forEach(function (key) {
     let element = JSON.parse(localStorage.getItem(key));
     let tr = document.createElement("tr");
+
+    // Create number
+    let number = document.createElement("th");
+    number.setAttribute("scope", "row");
+    number.appendChild(document.createTextNode(cnt));
+
     // Create author
     let title = document.createElement("td");
     title.appendChild(document.createTextNode(element.title));
@@ -37,15 +52,19 @@ function tableCreate() {
     pages.appendChild(document.createTextNode(element.pages));
 
     // Create status
-    let status = document.createElement("td");
+    let status = document.createElement("button");
+    status.setAttribute("id", `status-${key}`);
+    status.setAttribute("class", "btn btn-warning");
     status.appendChild(document.createTextNode(element.status));
 
     // Create button
     let button = document.createElement("button");
     button.setAttribute("id", key);
-    button.appendChild(document.createTextNode("Remove me"));
+    button.setAttribute("class", "btn btn-danger");
+    button.appendChild(document.createTextNode("X"));
 
     // Append to row
+    tr.appendChild(number);
     tr.appendChild(title);
     tr.appendChild(author);
     tr.appendChild(pages);
@@ -53,10 +72,29 @@ function tableCreate() {
     tr.appendChild(button);
 
     table.appendChild(tr);
+
+    // Event Listener for delete
     document.getElementById(key).addEventListener("click", function () {
       localStorage.removeItem(key);
       window.location.reload();
     });
+
+    // Event Listener for change status
+    document
+      .getElementById(`status-${key}`)
+      .addEventListener("click", function () {
+        updatedBook = new Book(
+          element.title,
+          element.author,
+          element.pages,
+          element.status
+        );
+        updatedBook.changeStatus();
+        localStorage.setItem(key, JSON.stringify(updatedBook));
+        window.location.reload();
+      });
+
+    cnt += 1;
   });
 }
 
